c3dad4f23a8f76b2938c39cfddb99dc896d09f22
muthiaadeis <muthia23ti@mahasiswa.pcr.ac.id>
2026-09-01 14:13:25 +0700

    sikit

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: '*');
+
         $middleware->alias([
             'auth.pasien' => \App\Http\Middleware\AuthPasien::class,
         ]);
